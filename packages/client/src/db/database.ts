548c2d1fa535e36a7611d431264f884eed0cbc63
oinsio <8233537+oinsio@users.noreply.github.com>
2026-06-24 11:51:36 +0500

    implement collapse-dexie-versions OpenSpec

--- packages/client/src/db/database.ts
+++ packages/client/src/db/database.ts
@@ -14,8 +14,6 @@ import type {
   Task,
 } from "@/types/entities";
 import { DB_SCHEMA } from "./schema";
-import { DB_SCHEMA_V1 } from "./schemaV1";
-import { upgradeSortOrderToFractional } from "./sortOrderMigration";
 
 export class ClearProgressDatabase extends Dexie {
   tasks!: EntityTable<Task, "id">;
@@ -32,19 +30,7 @@ export class ClearProgressDatabase extends Dexie {
 
   constructor() {
     super(DB_NAME);
-    this.version(1).stores(DB_SCHEMA_V1);
-    this.version(2)
-      .stores(DB_SCHEMA)
-      .upgrade(async (tx) => {
-        const coverRecords = await tx.table("covers").toArray();
-        const pendingCoverRecords = await tx.table("pending_covers").toArray();
-
-        await tx.table("files").bulkAdd(coverRecords);
-        await tx.table("pending_files").bulkAdd(pendingCoverRecords);
-      });
-    this.version(3)
-      .stores(DB_SCHEMA)
-      .upgrade((transaction) => upgradeSortOrderToFractional(transaction));
+    this.version(1).stores(DB_SCHEMA);
   }
 }
 
